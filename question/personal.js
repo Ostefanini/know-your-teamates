125387aaea0c7aeaebf85f515f0122b64e3f3bdc
Kevin Marcellier <kevin.marcellier@stack-labs.com>
2020-09-02 23:39:58 +0200

    feat(questions): Add some questions

--- question/personal.js
+++ question/personal.js
@@ -1,11 +1,35 @@
 var personalQuestions = [
-    "Café ou Thé ?",
-    "Ton alcool préféré ? (Ou tu bois pas?)",
+    // Gout
+    "Thé ou café ?",
+    "Ta boisson préférée ?",
     "Raclette ou Fondue ?",
-    "Quel Disney en #1 ?",
-    "Ton tout premier téléphone ?",
-    "Un résumé en 3 mots de Star Wars !",
+    "Ta couleur préférée ?",
     "Android ou iOS ?",
+    "As-tu une phobie ?",
+
+    // Vie personnelle
+    "Ton tout premier téléphone ?",
     "Ton dernier voyage ?",
-    "Marié, 2 enfants ?",
+    "Le plus beau pays que tu as visité ?",
+    "Marié(e), 2 enfants ?",
+    "Tu joues ou tu as joué d'un instrument de musique ?",
+    "Où es-tu né(e) ?",
+
+    // Cinéma
+    "Quel Disney en #1 ?",
+    "Un résumé en 3 mots de Star Wars !",
+    "Si tu ne devais garder que trois films...",
+    "Tu regardes une série en ce moment ?",
+    "Le dernier film que tu as vu au cinéma ?",
+
+    // Musique
+    "La chanson que tu écoutes le plus en ce moment ?",
+    "Ton dernier concert ?",
+
+    // Sport
+    "Tu pratiques un sport ?",
+    "Tu regardes du sport à la télé ?",
+
+    // Divers
+    "Raconte nous une blague !",
 ];
--- question/professional.js
+++ question/professional.js
@@ -1,6 +1,8 @@
 var professionalQuestions = [
-    "Quel est ton logiciel #1 au quotidien ?",
-    "Ton langage préféré ?",
+    "Quel est ton IDE #1 au quotidien ?",
+    "Quel est ton langage préféré ?",
     "Le langage avec lequel tu as commencé ?",
     "Tu as fais quoi comme études ?",
+    "Plutôt Front ou plutôt Back ?",
+    "Ta dernière réunion, c'était à quel sujet ?"
 ];
