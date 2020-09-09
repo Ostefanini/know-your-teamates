7cdef0e5ed81ab6e1d277eafb5bbe8e557bab5e8
Oscar Stefanini <oscar.stefanini1+gpg@gmail.com>
2020-09-09 14:53:28 +0200

    Update ib.js

--- question/ib.js
+++ question/ib.js
@@ -1,4 +1,6 @@
 var teamQuestions = [
     "Depuis combien de temps tu travailles pour InsideBoard ?",
     "Honnêtement, tu en penses quoi de ce orange pour IB ?",
+    "Comment est ce que tu as entendu parler d'InsideBoard ?",
+    "Sur quel projet d'InsideBoard tu préfères travailler ?",
 ];
