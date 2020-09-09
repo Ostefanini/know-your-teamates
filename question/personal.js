6a8cb54efbd6a9d1a4c6f29aab08bdd362c97214
Oscar Stefanini <oscar.stefanini1+gpg@gmail.com>
2020-09-09 15:01:36 +0200

    Update personal.js

--- question/personal.js
+++ question/personal.js
@@ -6,7 +6,12 @@ var personalQuestions = [
     "Ta couleur préférée ?",
     "Android ou iOS ?",
     "As-tu une phobie ?",
-
+    "Ton fromage préféré ?",
+    "Beurre doux, demi-sel ou salé ?",
+    "Chipolata ou merguez ou salade ?",
+    "Quel est ton soda préféré ?"
+    "Donne un de tes desserts préférés"
+    
     // Vie personnelle
     "Ton tout premier téléphone ?",
     "Ton téléphone actuel ?",
@@ -17,6 +22,8 @@ var personalQuestions = [
     "Où es-tu né(e) ?",
     "Tu habites où ?",
     "Ton dernier repas ?",
+    "Ton système d'exploitation préféré ?",
+    "Tu préfères un ordinateur portable ou un ordinateur fixe ?"
 
     // Cinéma
     "Quel Disney en #1 ?",
@@ -26,6 +33,9 @@ var personalQuestions = [
     "Le dernier film que tu as vu au cinéma ?",
     "Plutôt VF, VO ou VOST ?",
     "Cite nous 3 de tes séries préférées",
+    "Ton acteur ou actrice préféré(e) ?",
+    "Le type de film ou de série que tu préfères",
+    "le type de film ou de serie que tu n'aimes pas du tout",
 
     // Musique
     "La chanson que tu écoutes le plus en ce moment ?",
@@ -41,6 +51,6 @@ var personalQuestions = [
     "Ton jeu vidéo préféré ?",
 
     // Divers
-    "Si tu n'avais pas travailler dans l'informatique, tu aurais voulu faire quoi ?"
-
+    "Si tu n'avais pas travailler dans l'informatique, tu aurais voulu faire quoi ?",
+    "Est ce que tu collabores ou anime un projet personnel en dehors du boulot ? (tech ou pas)"
 ];
