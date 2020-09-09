ee05123c093c0c8cd4c1a17299c2c509968b3446
Oscar Stefanini <oscar.stefanini1+gpg@gmail.com>
2020-09-09 15:08:28 +0200

    Update professional.js

--- question/professional.js
+++ question/professional.js
@@ -5,5 +5,12 @@ var professionalQuestions = [
     "Tu as fais quoi comme études ?",
     "Plutôt Front ou plutôt Back ?",
     "Ta dernière réunion, c'était à quel sujet ?",
-    "Pour toi GIT c'est en ligne de commande ou tu utilises un GUI ?"
+    "Pour toi GIT c'est en ligne de commande ou tu utilises un GUI ?",
+    "Ton editeur de texte quand tu n'as pas d'interface graphique, vim, emacs ou nano ?",
+    "A quel moment tu t'es décidé(e) pour te lancer en informatique ?",
+    "Est ce que tu vis bien le télétravail ?",
+    "Est ce que tu as une préférence entre gitlab et github ?",
+    "Ton os préféré pour travailler",
+    "Linux/Macos, quel est le terminal que tu utilises au quotidien",
+    "Plutot souris ou Trackpad ?"
 ];
